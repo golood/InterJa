2d471d5d6411c864616bddf5561db4f8dccdad14
Andrey Shumilov <sumilovirk@gmail.com>
2018-09-18 13:41:48 +0800

    start project

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Файл для запуска
+'''
+
+string = '''int a = 5;
+            double b = 6,8;
+            string s = "str";
+            boolean bol = False;'''
+
---
+++ sitax.py
@@ -0,0 +1,12 @@
+'''
+Синтаксический анализатор
+'''
+
+
+
+class Operator:
+    def __init__(self, string):
+        self.string = string
+
+    def isType(self):
+        pass
